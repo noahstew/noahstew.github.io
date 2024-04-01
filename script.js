22f7a1a9a612aea32717be4208e03c2d7f502ac2
noahstew <njstewart22@icloud.com>
2024-04-01 09:50:13 -0700

    Contact Page Finished

--- script.js
+++ script.js
@@ -71,3 +71,52 @@ hamburger.addEventListener("click", () => {
     nameNeonText.classList.toggle("active");
     navBar.classList.toggle("active");
 });
+
+$('#email').hover(function () {
+    $(this).fadeOut(0, function () {
+        $(this).attr('src', 'icons/mail_yellow.png');
+        $(this).fadeIn(500);
+    });
+}, function () {
+    $(this).fadeOut(0, function () {
+        $(this).attr('src', 'icons/mail.png');
+        $(this).fadeIn(500);
+    });
+});
+
+$('#linkedin').hover(function () {
+    $(this).fadeOut(0, function () {
+        $(this).attr('src', 'icons/li_yellow.png');
+        $(this).fadeIn(500);
+    });
+}, function () {
+    $(this).fadeOut(0, function () {
+        $(this).attr('src', 'icons/li.png');
+        $(this).fadeIn(500);
+    });
+});
+
+$('#github').hover(function () {
+    $(this).fadeOut(0, function () {
+        $(this).attr('src', 'icons/github_yellow.png');
+        $(this).fadeIn(500);
+    });
+}, function () {
+    $(this).fadeOut(0, function () {
+        $(this).attr('src', 'icons/github.png');
+        $(this).fadeIn(500);
+    });
+});
+
+$('#instagram').hover(function () {
+    $(this).fadeOut(0, function () {
+        $(this).attr('src', 'icons/insta_yellow.png');
+        $(this).fadeIn(500);
+    });
+}, function () {
+    $(this).fadeOut(0, function () {
+        $(this).attr('src', 'icons/insta.png');
+        $(this).fadeIn(500);
+    });
+});
+
